1dcb4c00c43871ee6eb1d4f3538bfcb30e271080
shallwejazz-debug <shallwejazz@gmail.com>
2026-07-02 18:47:53 +0900

    Update renderer.tsx

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -15,6 +15,9 @@ export const renderer = jsxRenderer(({ children }) => {
       </head>
       <body>
         {children}
+        <footer class="site-footer">
+          <p>※ 이 사이트는 쿠팡 파트너스 등 제휴 마케팅 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받을 수 있습니다.</p>
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
         <script src="/static/app.js"></script>
       </body>
