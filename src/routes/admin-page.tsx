e68b24574b2bbe89d2d4e8b4ae72eaf707ebe76d
shallwejazz-debug <shallwejazz@gmail.com>
2026-07-21 21:53:58 +0900

    Update admin-page.tsx

--- src/routes/admin-page.tsx
+++ src/routes/admin-page.tsx
@@ -34,7 +34,7 @@ export function AdminPage(): string {
 
   <link href="/static/style.css" rel="stylesheet" />
   <link
-    href="/static/admin.css?v=20260721-event-groups-1"
+    href="/static/admin.css?v=20260721-watcher-transform-1"
     rel="stylesheet"
   />
 </head>
@@ -1527,7 +1527,7 @@ export function AdminPage(): string {
   </main>
 
     <script src="/static/admin.js?v=20260721-watcher-tab"></script>
-    <script src="/static/watcher-admin.js?v=20260721-event-groups-1"></script>
+    <script src="/static/watcher-admin.js?v=20260721-watcher-transform-1"></script>
 </body>
 </html>`
 }
